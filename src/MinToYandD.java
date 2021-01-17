8dd2edc35d8fd928243cc165e0fe07fbc309c635
Victor-Aremu <77357316+Victor-Aremu@users.noreply.github.com>
2021-01-17 16:25:17 +0000

    small changes made

--- src/MinToYandD.java
+++ src/MinToYandD.java
@@ -1,20 +1,14 @@
 public class MinToYandD {
-    public static void main(String[] args) {
-    printYearsAndDays(525600);
-
-    }
     public static void printYearsAndDays(long minutes){
-        long years = minutes / 525600;
-        long days = minutes % 1440;
-        long remainingMinutes = days % 525600;
-
-        //printYearsAndDays(525600);
+        long hours = minutes/60;
+        long days = hours / 24;
+        long years = days / 365;
+        long remainingMinutes = days % 365;
         if (minutes < 0) {
             System.out.println("Invalid Value");
         }
         else {
-            System.out.println(minutes + "mins = " + years + "y and " + days +"d");
-            System.out.println(remainingMinutes);
+            System.out.println(minutes + " mins = " + years + " y and " + remainingMinutes +" d");
         }
     }
 }
